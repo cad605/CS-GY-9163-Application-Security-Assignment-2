44f4c4ae1f39fbd832f9bd35130afea79d0f124b
cad605 <cad605@nyu.edu>
2021-07-09 20:35:02 -0400

    Part 2 Completed.

--- GiftcardSite/GiftcardSite/settings.py
+++ GiftcardSite/GiftcardSite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#'kmgysa#fz+9(z1*=c0ydrjizk*7sthm2ga1z4=^61$cxcq8b$l'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
